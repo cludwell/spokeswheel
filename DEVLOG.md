72238dd2818e5c4f8499a0f1704befaba01bbe23
cludwell <cludwell@gmail.com>
2024-03-01 22:32:20 -0800

    first commit

---
+++ DEVLOG.md
@@ -0,0 +1,3 @@
+Get Prisma installed
+Try using Next font imports from google
+Splash page
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,7 @@ module.exports = {
     },
   },
   plugins: [],
+  daisyui: {
+    themes: ["light", "dark", "dim"],
+  },
 };

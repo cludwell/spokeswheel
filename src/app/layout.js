c992416143e5906a3a92042412485e02c9c3478b
cludwell <cludwell@gmail.com>
2024-03-02 23:24:50 -0800

    prisma file

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
-import Head from "next/head";
 export const metadata = {
   title: "SpokesWheel Conference",
   description: "A spiritual conference for dissidents",
@@ -12,14 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="icon"
-          href="/images/wheel.png"
-          type="image/png"
-          sizes="any"
-        />
-      </Head>
+
       <body className={inter.className} data-theme="dark">
         <Header />
         {children}

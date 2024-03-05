75d52ecdc0e94c15d0fbcd0b02c63349e2f6146e
cludwell <cludwell@gmail.com>
2024-03-05 15:25:22 -0800

    registration form

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -58,13 +58,13 @@ export default function Register() {
     if (name === "emergencyNumber") {
       // Only format if the input name is 'emergencyNumber'
       const formattedPhoneNumber = formatPhoneNumber(value);
-      setUserData(prevState => ({
+      setUserData((prevState) => ({
         ...prevState,
-        [name]: formattedPhoneNumber // Use the formatted number
+        [name]: formattedPhoneNumber, // Use the formatted number
       }));
     } else {
       // For all other inputs, use the value as-is
-      setUserData(prevState => ({
+      setUserData((prevState) => ({
         ...prevState,
         [name]: value,
       }));
@@ -91,7 +91,7 @@ export default function Register() {
         }
       >
         <h2 className={amatic.className + " mb-12 text-5xl"}>
-          2024 Registration
+          2024 Registration At Camp Seawood!
         </h2>
         {booked ? (
           <div className="w-full flex flex-col items-center justify-center">
@@ -129,19 +129,74 @@ export default function Register() {
               placeholder="(123) 456-7890"
               maxLength={14}
               required
-              className="input input-bordered input-primary w-full max-w-xs"
+              className="input input-bordered input-secondary w-full max-w-xs"
             />
-            <label className=" font-bold text-xl" htmlFor="emergencyName">
+            <label className=" font-bold text-xl" htmlFor="emergencyRelation">
               - Relation
             </label>
             <input
               type="text"
-              name="emergencyName"
-              id="emergencyName"
-              value={emergencyName}
+              name="emergencyRelation"
+              id="emergencyRelation"
+              value={emergencyRelation}
               onChange={handleChange}
               required
-              className="input input-bordered input-primary w-full max-w-xs"
+              className="input input-bordered input-accent w-full max-w-xs"
+            />
+            <label className=" font-bold text-xl" htmlFor="dietaryRestrictions">
+              Dietary Restrictions
+            </label>
+
+            <select
+              className="select select-info w-full max-w-xs"
+              type="text"
+              name="dietaryRestrictions"
+              id="dietaryRestrictions"
+              value={dietaryRestrictions}
+              onChange={handleChange}
+            >
+              <option value={"Omnivore"}>Omnivore</option>
+              <option value={"Vegan"}>Vegan</option>
+              <option value={"Vegetarian"}>Vegetarian</option>
+            </select>
+            <label className=" font-bold text-xl" htmlFor="allergies">
+              Allergies
+            </label>
+            <textarea
+              type="text"
+              name="allergies"
+              id="allergies"
+              value={allergies}
+              onChange={handleChange}
+              placeholder="Are there any allergies that we should know about? Foods you cannot eat? Medications not to be given in an emergency? If not, you can just write - N/A"
+              className="textarea textarea-warning min-h-40 w-80"
+            />
+            <label
+              className=" font-bold text-xl"
+              htmlFor="specialAccomodations"
+            >
+              Special Accomodations
+            </label>
+            <textarea
+              type="text"
+              name="specialAccomodations"
+              id="specialAccomodations"
+              value={specialAccomodations}
+              onChange={handleChange}
+              placeholder="Are there any special accomodations we can try to make for you? Is there medication you need refrigerated? Accessibility concerns?"
+              className="textarea textarea-error min-h-40 w-80"
+            />
+            <label className=" font-bold text-xl" htmlFor="notes">
+              Notes
+            </label>
+            <textarea
+              type="text"
+              name="notes"
+              id="notes"
+              value={notes}
+              onChange={handleChange}
+              placeholder="Is there anything else you'd like us to know? Could you use a ride from Logan Airport? "
+              className="textarea textarea-primary min-h-40 w-80"
             />
             <label className=" font-bold text-xl" htmlFor="photoConsent">
               Photo Consent

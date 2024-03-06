b6b50a98ab001138eb83497c588f9dfa4ec702c9
cludwell <cludwell@gmail.com>
2024-03-05 16:04:13 -0800

    registration error handling

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Special_Elite, Amatic_SC } from "next/font/google";
+import IconExclamation from "@/components/Icons/IconExclamation";
 const special = Special_Elite({
   weight: "400",
   subsets: ["latin"],
@@ -20,7 +21,7 @@ export default function Register() {
     emergencyRelation: "",
     emailList: false,
     textUpdates: false,
-    dietaryRestrictions: "",
+    dietaryRestrictions: "Please make a selection",
     allergies: "",
     notes: "",
     specialAccomodations: "",
@@ -74,18 +75,21 @@ export default function Register() {
     const err = {};
     if (!emergencyName || emergencyName.length < 6)
       err.emergencyName = "Please enter a name for emergency contact.";
-    if (!emergencyNumber || emergencyNumber.length < 10)
+    if (!emergencyNumber || emergencyNumber.length < 14)
       err.emergencyNumber = "Please enter a valid phone number.";
     if (!emergencyRelation || emergencyRelation.length < 3)
       err.emergencyRelation =
         "Please let us know your relationship to this person.";
+    if (dietaryRestrictions.length < 5)
+      err.dietaryRestrictions = "Please make a diet selection";
     setErrors(err);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     validate();
-    console.log("userdata", userData);
+    if (Object.values(errors).length) return;
+    else console.log("userdata", userData);
   };
   let booked = false;
 
@@ -96,7 +100,7 @@ export default function Register() {
           special.className + " p-16 bg-black max-w-screen-xl mx-auto leading-8"
         }
       >
-        <h2 className={amatic.className + " mb-12 text-5xl"}>
+        <h2 className={amatic.className + " mb-12 text-5xl fade-in"}>
           2024 Registration At Camp Seawood!
         </h2>
         {booked ? (
@@ -104,143 +108,182 @@ export default function Register() {
             <h2 className={amatic.className + " mb-12 text-5xl fade-in"}>
               {`✅We'll see you there!`}
             </h2>
-            <p>{`Your registration is complete! We're looking forward to seeing you at the conference!`}</p>
+            <p className="fade-in">{`Your registration is complete! We're looking forward to seeing you at the conference!`}</p>
           </div>
         ) : (
-          <form className=" flex flex-col items-center mx-auto w-3/4 " onSubmit={handleSubmit}>
+          <form
+            className=" flex flex-col items-center mx-auto w-3/4 fade-in"
+            onSubmit={handleSubmit}
+          >
             <div className="grid grid-cols-2 gap-4">
+              <div className=" text-2xl">Emergency Contact </div>
+              <div></div>
+              <label className=" font-bold text-xl" htmlFor="emergencyName">
+                - Name
+              </label>
+              <div>
+                <input
+                  type="text"
+                  name="emergencyName"
+                  id="emergencyName"
+                  value={emergencyName}
+                  onChange={handleChange}
+                  required
+                  className="input input-bordered input-primary w-full max-w-xs"
+                />
+                {errors && errors.emergencyName && (
+                  <div className=" bg-red-300 text-red-950 rounded-2xl my-3 flex flex-row p-3 fade-in w-80">
+                    <IconExclamation /> {errors.emergencyName}
+                  </div>
+                )}
+              </div>
+              <label className=" font-bold text-xl" htmlFor="emergencyNumber">
+                - Number
+              </label>
+              <div>
+                <input
+                  type="tel"
+                  name="emergencyNumber"
+                  id="emergencyNumber"
+                  value={emergencyNumber}
+                  onChange={handleChange}
+                  pattern="\d{10"
+                  placeholder="(123) 456-7890"
+                  maxLength={14}
+                  required
+                  className="input input-bordered input-secondary w-full max-w-xs"
+                />
+                {errors && errors.emergencyNumber && (
+                  <div className="bg-red-300 text-red-950 rounded-2xl my-3 flex flex-row p-3 fade-in w-80">
+                    <IconExclamation /> {errors.emergencyNumber}
+                  </div>
+                )}
+              </div>
 
-            <div className=" text-2xl">Emergency Contact </div>
-            <div></div>
-            <label className=" font-bold text-xl" htmlFor="emergencyName">
-              - Name
-            </label>
-            <input
-              type="text"
-              name="emergencyName"
-              id="emergencyName"
-              value={emergencyName}
-              onChange={handleChange}
-              required
-              className="input input-bordered input-primary w-full max-w-xs"
-            />
-            <label className=" font-bold text-xl" htmlFor="emergencyNumber">
-              - Number
-            </label>
-            <input
-              type="tel"
-              name="emergencyNumber"
-              id="emergencyNumber"
-              value={emergencyNumber}
-              onChange={handleChange}
-              pattern="\d{10"
-              placeholder="(123) 456-7890"
-              maxLength={14}
-              required
-              className="input input-bordered input-secondary w-full max-w-xs"
-            />
-            <label className=" font-bold text-xl" htmlFor="emergencyRelation">
-              - Relation
-            </label>
-            <input
-              type="text"
-              name="emergencyRelation"
-              id="emergencyRelation"
-              value={emergencyRelation}
-              onChange={handleChange}
-              required
-              className="input input-bordered input-accent w-full max-w-xs"
-            />
-            <label className=" font-bold text-xl mt-8" htmlFor="dietaryRestrictions">
-              Dietary Restrictions
-            </label>
-
-            <select
-              className="select select-info w-full max-w-xs mt-8"
-              type="text"
-              name="dietaryRestrictions"
-              id="dietaryRestrictions"
-              value={dietaryRestrictions}
-              onChange={handleChange}
-            >
-              <option value={"Omnivore"}>Omnivore</option>
-              <option value={"Vegan"}>Vegan</option>
-              <option value={"Vegetarian"}>Vegetarian</option>
-            </select>
-            <label className=" font-bold text-xl" htmlFor="allergies">
-              Allergies
-            </label>
-            <textarea
-              type="text"
-              name="allergies"
-              id="allergies"
-              value={allergies}
-              onChange={handleChange}
-              placeholder="Are there any allergies that we should know about? Foods you cannot eat? Medications not to be given in an emergency? If not, you can just write - N/A"
-              className="textarea textarea-warning min-h-40 w-80"
-            />
-            <label
-              className=" font-bold text-xl"
-              htmlFor="specialAccomodations"
-            >
-              Special Accomodations
-            </label>
-            <textarea
-              type="text"
-              name="specialAccomodations"
-              id="specialAccomodations"
-              value={specialAccomodations}
-              onChange={handleChange}
-              placeholder="Are there any special accomodations we can try to make for you? Is there medication you need refrigerated? Accessibility concerns?"
-              className="textarea textarea-error min-h-40 w-80"
-            />
-            <label className=" font-bold text-xl" htmlFor="notes">
-              Notes
-            </label>
-            <textarea
-              type="text"
-              name="notes"
-              id="notes"
-              value={notes}
-              onChange={handleChange}
-              placeholder="Is there anything else you'd like us to know? Could you use a ride from Logan Airport? "
-              className="textarea textarea-primary min-h-40 w-80"
-            />
-            <label className=" font-bold text-xl" htmlFor="photoConsent">
-              Photo Consent
-            </label>
-            <input
-              type="checkbox"
-              className="toggle toggle-success"
-              name="photoConsent"
-              id="photoConsent"
-              value={photoConsent}
-              onChange={handleChange}
-            />
-            <label className=" font-bold text-xl" htmlFor="textUpdates">
-              Text Updates
-            </label>
-            <input
-              type="checkbox"
-              className="toggle toggle-warning"
-              name="textUpdates"
-              id="textUpdates"
-              value={textUpdates}
-              onChange={handleChange}
-            />
-            <label className=" font-bold text-xl" htmlFor="emailList">
-              Email List
-            </label>
-            <input
-              type="checkbox"
-              className="toggle toggle-info"
-              name="emailList"
-              id="emailList"
-              value={emailList}
-              onChange={handleChange}
-            />
+              <label className=" font-bold text-xl" htmlFor="emergencyRelation">
+                - Relation
+              </label>
+              <div>
+                <input
+                  type="text"
+                  name="emergencyRelation"
+                  id="emergencyRelation"
+                  value={emergencyRelation}
+                  onChange={handleChange}
+                  required
+                  className="input input-bordered input-accent w-full max-w-xs"
+                />
+                {errors && errors.emergencyRelation && (
+                  <div className=" bg-red-300 text-red-950 rounded-2xl my-3 flex flex-row p-3 fade-in w-80">
+                    <IconExclamation /> {errors.emergencyRelation}
+                  </div>
+                )}
+              </div>
+              <label
+                className=" font-bold text-xl mt-8"
+                htmlFor="dietaryRestrictions"
+              >
+                Dietary Restrictions
+              </label>
+              <div>
+                <select
+                  className="select select-info w-full max-w-xs mt-8"
+                  type="text"
+                  name="dietaryRestrictions"
+                  id="dietaryRestrictions"
+                  value={dietaryRestrictions}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled selected>
+                    Please make a selection
+                  </option>{" "}
+                  <option value={"Omnivore"}>Omnivore</option>
+                  <option value={"Vegan"}>Vegan</option>
+                  <option value={"Vegetarian"}>Vegetarian</option>
+                </select>
+                {errors && errors.dietaryRestrictions && (
+                  <div className=" bg-red-300 text-red-950 rounded-2xl my-3 flex flex-row p-3 fade-in w-80">
+                    <IconExclamation /> {errors.dietaryRestrictions}
+                  </div>
+                )}
+              </div>
+              <label className=" font-bold text-xl" htmlFor="allergies">
+                Allergies
+              </label>
+              <textarea
+                type="text"
+                name="allergies"
+                id="allergies"
+                value={allergies}
+                onChange={handleChange}
+                placeholder="Are there any allergies that we should know about? Foods you cannot eat? Medications not to be given in an emergency? If not, you can just write - N/A"
+                className="textarea textarea-warning min-h-40 w-80"
+              />
+              <label
+                className=" font-bold text-xl"
+                htmlFor="specialAccomodations"
+              >
+                Special Accomodations
+              </label>
+              <textarea
+                type="text"
+                name="specialAccomodations"
+                id="specialAccomodations"
+                value={specialAccomodations}
+                onChange={handleChange}
+                placeholder="Are there any special accomodations we can try to make for you? Is there medication you need refrigerated? Accessibility concerns?"
+                className="textarea textarea-error min-h-40 w-80"
+              />
+              <label className=" font-bold text-xl" htmlFor="notes">
+                Notes
+              </label>
+              <textarea
+                type="text"
+                name="notes"
+                id="notes"
+                value={notes}
+                onChange={handleChange}
+                placeholder="Is there anything else you'd like us to know? Could you use a ride from Logan Airport? "
+                className="textarea textarea-primary min-h-40 w-80"
+              />
+              <label className=" font-bold text-xl" htmlFor="photoConsent">
+                Photo Consent
+              </label>
+              <input
+                type="checkbox"
+                className="toggle toggle-success"
+                name="photoConsent"
+                id="photoConsent"
+                value={photoConsent}
+                onChange={handleChange}
+              />
+              <label className=" font-bold text-xl" htmlFor="textUpdates">
+                Text Updates
+              </label>
+              <input
+                type="checkbox"
+                className="toggle toggle-warning"
+                name="textUpdates"
+                id="textUpdates"
+                value={textUpdates}
+                onChange={handleChange}
+              />
+              <label className=" font-bold text-xl" htmlFor="emailList">
+                Email List
+              </label>
+              <input
+                type="checkbox"
+                className="toggle toggle-info"
+                name="emailList"
+                id="emailList"
+                value={emailList}
+                onChange={handleChange}
+              />
             </div>
-            <button className="btn btn-accent btn-wide mb-4 text-xl mt-12" type="submit">
+            <button
+              className="btn btn-accent btn-wide mb-4 text-xl mt-12"
+              type="submit"
+            >
               Submit
             </button>
           </form>

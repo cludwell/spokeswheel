9e932ac562e3893a609b32027726edef5f8234b1
cludwell <cludwell@gmail.com>
2024-03-19 20:24:44 -0700

    clean up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,8 @@ import Image from "next/image";
 import darkCampFire from "../../public/images/dark-campfire.jpg";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
-import LogInModal from "./LogInModal";
-import SignUpModal from "./SignUpModal";
 import { signOut, useSession } from "next-auth/react";
 import { useStore } from "@/store/ZustandProvider";
-import IconUser from "./Icons/IconUser";
-import IconGear from "./Icons/IconGear";
 import Logo from "./Logo";
 import ConferenceName from "./ConferenceName";
 import { gloria } from "../app/fonts";
@@ -24,24 +20,7 @@ export default function Header() {
       fetchUsersBookings: state.fetchUsersBookings,
     }));
   const [loaded, setLoaded] = useState(false);
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
 
-  useEffect(() => {
-    if (!showMenu) return;
-    const closeMenu = (e) => {
-      if (!ulRef.current?.contains(e.target)) {
-        setShowMenu(false);
-      }
-    };
-    document.addEventListener("click", closeMenu);
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
-  const closeMenu = (e) => setShowMenu(false);
   useEffect(() => setLoaded(true), []);
   useEffect(() => {
     const loadUser = async () => {
@@ -51,13 +30,6 @@ export default function Header() {
     if (session) loadUser();
   }, [session, fetchUserData, fetchUsersBookings]);
 
-  const handleSignOut = async () => {
-    const data = await signOut({ redirect: true, callbackUrl: "/" });
-    await dismissUserData();
-  };
-  const seawood = bookings.length
-    ? bookings.filter((b) => b.conferenceId == 1)[0]
-    : null;
   return (
     <div
       className="flex flex-col items-center self-center w-screen max-w-screen-xl mx-auto h-fit"
@@ -89,72 +61,6 @@ export default function Header() {
         <li className="text-lg btn btn-ghost">
           <Link href={"/contact"}>contact</Link>
         </li>
-        {/* {!session ? (
-          <>
-            <div>
-              <LogInModal />
-            </div>
-            <div>
-              <SignUpModal />
-            </div>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link href={"/registration"}>register</Link>
-            </li>
-            <div
-              onClick={openMenu}
-              className="relative flex flex-row items-center "
-            >
-              <span className="cursor-pointer md:scale-125 btn btn-ghost">
-                <IconGear />
-              </span>
-              <div
-                className={`${
-                  gloria.className
-                } absolute right-4 top-4 sm:top-16 bg-base-200 rounded-xl drop-shadow-2xl p-4 transition ease-in-out duration-400 w-fit z-10 border-2  border-slate-700  ${
-                  showMenu ? " scale-75 sm:scale-100" : " scale-0"
-                } `}
-                ref={ulRef}
-              >
-                <div className="flex flex-row ml-4 text-xl ">
-                  <IconUser />{" "}
-                  <span className="ml-4 ">
-                    {user?.firstName} {user?.lastName}
-                  </span>
-                  <div className="divider"></div>
-                </div>
-                <div className="flex flex-col w-full h-4 my-0">
-                  <div className="divider"></div>
-                </div>
-                <ul className="w-64 gap-4 sm:text-xl menu bg-base-200 rounded-box">
-                  <li>
-                    <Link href={"/updateinfo"} onClick={closeMenu}>
-                      Update User Info
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href={"/registration/update"} onClick={closeMenu}>
-                      Update Registration
-                    </Link>
-                  </li>
-                  {!!bookings.length && (
-                    <li>
-                      <Link
-                        href={"/registration/cancel"}
-                        onClick={closeMenu}
-                      >{`Cancel Registration `}</Link>
-                    </li>
-                  )}
-                  <li onClick={handleSignOut} className="text-xl btn btn-info">
-                    Log out
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </>
-        )} */}
         <DropDownMenu />
       </ul>
     </div>

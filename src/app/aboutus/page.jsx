487f11e59e093b85456b8669dfde58315b24c59c
cludwell <cludwell@gmail.com>
2024-03-24 22:09:39 -0700

    language, layout, editting

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -11,12 +11,12 @@ export default function AboutUs() {
       </h2>
       <p
         className={" my-8 fade-in"}
-      >{`We are a group of radicals and leftists who grew up in Unitarian Universalist (UU) or UU adjacent spaces and conferences. Disillusioned by that community, by UUism, which seemed like virtue signaling without principal, we as adults have struggled to find spiritual spaces that do not bear the authoritarianism of organized religion, celebrate spiritual bypassing (using spiritual ideas and practices to avoid facing psychological wounds caused by capitalism, the state, patriarchy, etc.) or the appropriative practices of what passes as "spirituality." You won't hear us saying corny shit like "low vibration frequency" 🤨. What the heck even is that?`}</p>
+      >{`We are a group of left leaning folks who grew up in Unitarian Universalist (UU) or UU adjacent spaces and conferences. Disillusioned by that community, we as adults have struggled to find spiritual spaces that do not bear the authoritarianism of organized religion, celebrate spiritual bypassing (using spiritual ideas and practices to avoid facing psychological wounds caused by capitalism, the state, patriarchy, etc.) or the appropriative practices of what passes as "spirituality." You won't hear us saying corny stuff like "low vibration frequency" 🤨. What the heck even is that?`}</p>
 
       <p className="my-8 fade-in">
-        We are trying to build an intentional spiritual community, across
+        {`We are trying to build an intentional spiritual community, across
         generations, that is based on mutual aid, anti-oppression, and social
-        solidarity. We embody this through :
+        solidarity. We embody this through :`}
       </p>
 
       <ul className="ml-4 fade-in">
@@ -52,34 +52,41 @@ export default function AboutUs() {
       <h2 className={amatic.className + " my-12 text-5xl fade-in"}>
         What To Expect
       </h2>
-      <p className="my-8 ">{`This community is what we make of it. As this will be our first conference, we're still imagining what this could be. We've floated ideas of traveling the country together, going on a backpacking trip through a national park to an isolated hot spring, or staying in a haunted hotel on an island. Everything here is a living document open to change with your participation. The general structure to expect is: `}</p>
+      <p className="my-8 ">{`This community is what we make of it. Think of it as a stone soup, where everyone is contributing brings something to contribute. As this will be our first conference, we're still imagining what this could be. We've floated ideas of traveling the country together, going on a backpacking trip through a national park to an isolated hot spring, or staying in a haunted hotel on an island. Everything here is a living document open to change with your participation. The general structure to expect is: `}</p>
       <ul className="ml-4">
         <li className="flex flex-row">
-          <span className="w-12 text-center">🏕</span>
-          Summer camp, if summer camp were punk as fuck.
+          <div className="w-6 mx-2 ">👨🏼🏕</div>
+          <div className="w-12 text-center">
+            Summer camp for all ages if summer camp were punk rock.
+          </div>
         </li>
         <li className="flex flex-row">
-          <div className="w-12 text-center ">👨🏼‍🍳</div>
+          <div className="w-6 mx-2 ">👨🏼‍🍳</div>
           <div>
-            {" "}
             {`Cooking giant amounts of food and eating collective meals together.`}
           </div>
         </li>
         <li className="flex flex-row">
-          <div className="w-12 text-center">👩🏼‍🏫</div>
-          {`Workshops on things we're passionate about during the day.`}
+          <div className="w-6 mx-2 ">👩🏼‍🏫</div>
+          <div>
+            {`Workshops on things we're passionate about during the day.`}
+          </div>
         </li>
         <li className="flex flex-row">
-          <div className="w-12 text-center">🧘‍♂️</div>
-          {`Evening worship, some reflection about this human experience, completely voluntary.`}
+          <div className="w-6 mx-2 ">🧘‍♂️</div>
+          <div>
+            {`Evening worship, some reflection about this human experience, completely voluntary.`}
+          </div>
         </li>
         <li className="flex flex-row">
-          <div className="w-12 text-center ">👨‍👩‍👦‍👦</div>
-          Break out groups, called touch groups, to foster building connections
-          with other conferees.
+          <div className="w-6 mx-2 ">👨‍👩‍👦‍👦</div>
+          <div>
+            {`Break out groups, called family groups, to foster building connections
+          with other conferees.`}
+          </div>
         </li>
         <li className="flex flex-row">
-          <div className="w-12 px-4 text-center ">🎸</div>
+          <div className="w-6 mx-2 ">🎸</div>
           <div>
             {`A coffee house at the end of the conference, a time to play music, sing, or share some other performance, completely voluntary. In the past this has looked like a punk show in the middle of the woods.`}
           </div>
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -9,46 +9,147 @@ export default function ContactUs() {
         Contact Us
       </h2>
       <p className="my-8 ">
-        {`Questions? Suggestions? Would you like to participate in the planning? We need your help! We can't emphasis enough that this conference is what each one of us brings to it. Please think about planning a workshop, a worship, or start practicing a few songs to perform. As an organization we're rather ad hoc at the moment but you're welcome to reach out about joining our meetings if interested. If you would like to participate we need help with the following roles.`}
+        {`Questions? Suggestions? Would you like to participate in the planning? We need your help! We can't emphasis enough that this conference is what each one of us brings to it. Please think about planning a workshop, a meditation/worship, or start practicing a few songs to perform. As an organization we're rather ad hoc at the moment but you're welcome to reach out about joining our meetings if interested. If you would like to participate we need help with the following roles.`}
       </p>
       <ul className="m-8 fade-in">
         <li className="list-disc ">{`Yearbook Coordinator - Do you have a DSLR? Any interest in documenting the growth of this community?`}</li>
-        <li className="list-disc ">Member at Large - Would handle various oddball tasks</li>
+        <li className="list-disc ">
+          Member at Large - Would handle various oddball tasks
+        </li>
       </ul>
       <h2 className={amatic.className + " mb-12 text-5xl fade-in"}>Website </h2>
       <p className="my-8 ">
-        For any technical problems with registration or the website, please
-        email me at cludwell@gmail.com
+        {`For any technical problems with registration or the website, please
+        email me at `}
+        <a className="link link-error" href="mailto:cludwell@gmail.com">
+          cludwell@gmail.com
+        </a>
       </p>
       <h2 className={amatic.className + " mb-12 text-5xl fade-in"}>
         Workshop Coordinator
       </h2>
       <p className="my-8 ">
-        {`I believe that at the moment Sarah Mc is our workshop coordinator. The workshops that will be available will be the ones that attendees plan! So please think if there is something you would like to share? A report back from an important demonstration or protest? A history of a niche subject? Making pottery from foraged clay? `}
+        {`Sarah Mc is our workshop coordinator. The workshops that will be available will be the ones that attendees plan! So please think if there is something you would like to share? A report back from an important demonstration or protest? A history of a niche subject? Making pottery from foraged clay? `}
       </p>
       <p className="my-8 ">
-        {`You can reach Sarah at smcmullen719@gmail.com.`}
+        {`You can reach Sarah at `}
+        <a className="link link-primary " href="mailto:smcmullen719@gmail.com">
+          smcmullen719@gmail.com
+        </a>
       </p>
       <h2 className={amatic.className + " mb-12 text-5xl fade-in"}>
         Reflections Coordinator
       </h2>
       <p className="my-8 ">
-        {`Each night, we're planning on having guided meditations, reflections, or some spaces we've been in have called them worships. If this sounds weird, we encourage you to check it out at lease once. They're totally voluntary. These require community input! If you or a few others would like to lead a reflection please reach out to Meghan, our Reflections Coordinator.`}
+        {`Each night, we're planning on having guided meditations, reflections, or some spaces we've been in have called them worships. If this sounds weird, we encourage you to check it out at lease once. They're totally voluntary. These also require community input! If you or a few others would like to lead a reflection please reach out to Meghan, our Reflections Coordinator.`}
       </p>
       <p className="my-8 ">
-        {`Meghan can be reached at mkguilfoyle90@gmail.com`}
+        {`Meghan can be reached at `}
+        <a className="link link-accent" href="mailto:mkguilfoyle90@gmail.com">
+          mkguilfoyle90@gmail.com
+        </a>
       </p>
       <h2 className={amatic.className + " mb-12 text-5xl"}>Meal Coordinator</h2>
       <p className="my-8 ">
-        {`Jared will be coordinating food for us! If you have any dietary restrictions or concerns you can reach Jared at jareddefrancesco@gmail.com`}
+        {`Jared will be coordinating food for us! If you have any dietary restrictions or concerns you can reach Jared at `}
+        <a
+          className="link link-warning"
+          href="mailto:jareddefrancesco@gmail.com"
+        >
+          jareddefrancesco@gmail.com
+        </a>
       </p>
-      <h2 className={amatic.className + " mb-12 text-5xl"}>Family Group Coordinator</h2>
+      <h2 className={amatic.className + " mb-12 text-5xl"}>
+        Family Group Coordinator
+      </h2>
       <p className="my-8 ">
         {`We also need volunteers for people to lead family groups! If you have any experience or interest please reach out to Matty! Family groups are a space to break the ice with other conferees.`}
       </p>
       <p className="my-8 ">
-        {`Please reach out to Matty at matthewpatrickmorin@gmail.com`}
+        {`Please reach out to Matty at `}
+        <a
+          className="link link-secondary"
+          href="mailto:matthewpatrickmorin@gmail.com"
+        >
+          matthewpatrickmorin@gmail.com
+        </a>
       </p>
+      <div className="flex flex-col items-center">
+        <table className="table-xs sm:table-sm md:table-lg table-zebra bg-base-300 rounded-xl">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Title</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td>Sarah</td>
+              <td>Workshops</td>
+              <td>
+                <a
+                  className="link link-primary "
+                  href="mailto:smcmullen719@gmail.com"
+                >
+                  smcmullen719@gmail.com
+                </a>
+              </td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <td>Matty Mo</td>
+              <td>Family Groups</td>
+              <td>
+                <a
+                  className="link link-secondary"
+                  href="mailto:matthewpatrickmorin@gmail.com"
+                >
+                  matthewpatrickmorin@gmail.com
+                </a>
+              </td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <td>Meghan</td>
+              <td>Reflections</td>
+              <td>
+                <a
+                  className="link link-accent"
+                  href="mailto:mkguilfoyle90@gmail.com"
+                >
+                  mkguilfoyle90@gmail.com
+                </a>
+              </td>
+            </tr>
+            {/* row 4 */}
+            <tr>
+              <td>Jared</td>
+              <td>Food</td>
+              <td>
+                <a
+                  className="link link-warning"
+                  href="mailto:jareddefrancesco@gmail.com"
+                >
+                  jareddefrancesco@gmail.com
+                </a>
+              </td>
+            </tr>
+            {/* row 5 */}
+            <tr>
+              <td>Chris</td>
+              <td>Website</td>
+              <td>
+                <a className="link link-error" href="mailto:cludwell@gmail.com">
+                  cludwell@gmail.com
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }

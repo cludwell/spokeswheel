6aca81c56f0de1de7e3b19967c4d1d92913608ff
cludwell <cludwell@gmail.com>
2024-12-28 16:10:41 -0800

    creating plans dropdown

---
+++ src/components/DropDownPlans.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import { gloria } from "@/app/fonts";
+import { motion } from "framer-motion";
+export default function DropDownPlans() {
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+    const closeMenu = (e) => {
+      if (!ulRef.current?.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+    document.addEventListener("click", closeMenu);
+    return () => document.removeEventListener('click', closeMenu)
+  }, (showMenu));
+  const closeMenu = e => setShowMenu(false);
+
+  return (
+    <>
+      <div className="relative">
+        <span
+        className={`${gloria.className} scale-125 cursor-pointer md: btn btn-ghost`}
+        onClick={openMenu}>
+            plans
+        </span>
+        <motion.div
+    className=``
+
+      </div>
+    </>
+  );
+}
